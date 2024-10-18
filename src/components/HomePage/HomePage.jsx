ad0f259cc32f84d0a8c85bd4949c5c71a34d48c7
PhanAnhHao <pdkenny25@gmail.com>
2024-10-18 21:27:46 +0700

    HomePageNews

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,8 +1,10 @@
 import HomePageBanner from "./HomePageBanner/HomePageBanner";
+import HomePageNews from "./HomePageNews/HomePageNews";
 const HomePage = () => {
     return (
         <div>
             <HomePageBanner />
+            <HomePageNews />
         </div>
     );
 };
--- src/components/HomePage/HomePageBanner/HomePageBanner.jsx
+++ src/components/HomePage/HomePageBanner/HomePageBanner.jsx
@@ -4,13 +4,13 @@ import "./HomePageBanner.scss";
 
 const HomePageBanner = () => {
     return (
-        <div className="homeBanner-container mb-6">
+        <div className="homeBanner-container mb-12">
             <img src={HomePageBannerImg} className="homeBanner-img" alt="" />
             <div className="homeBanner-content">
                 <div className="text-5xl font-bold text-blue-900">
                     Choose <p className="text-[#1a96de]">study aboard or job aboard</p> suit for you
                 </div>
-                <div className="text-2xl font-semibold text-blue-700 text-wrap w-1/2 mt-6 mb-6">
+                <div className="text-2xl font-normal text-blue-900 text-wrap w-1/2 mt-6 mb-6">
                     We always make our customer happy by providing
                     as many choices as possible
                 </div>
